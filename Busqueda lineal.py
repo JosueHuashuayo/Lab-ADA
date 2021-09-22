756706829850b056b77582bc877b318c019e4c29
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2021-09-21 23:34:55 -0500

    pasando todo a un archivo , calculando tiempo computacional

--- Busqueda lineal.py
+++ Busqueda lineal.py
@@ -1,9 +1,34 @@
-
-array=[1,2,3]
-numero=1
+import time
+import random
+from typing import final
 def Busqueda(array, numero):
     for n in range(len(array)):
-        if array[n]==numero:
+        if array[n] == numero:
             print("lo encontro")
-            
-Busqueda(array,numero)
+
+def insertionSort(array):
+    for i in range(1,len(array)):
+        aux = array[i]
+        j = i-1
+        while j >= 0 and array[j] > aux :
+                array[j + 1] = array[j]
+                j = j - 1
+        array[j + 1] = aux
+    print(array)
+
+def n_values(n):
+    array=[ ]
+    for i in range(n):
+        array.append(random.randint(1,n))
+    return array
+
+inicio = time.time()
+array = n_values(10000)      
+# Busqueda(array,numero)
+insertionSort(array)
+final = time.time()
+
+print(final-inicio)
+
+
+
