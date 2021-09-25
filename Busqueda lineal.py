2882677cc4efb9956384552a5b509bdffc2dff7d
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2021-09-25 15:00:02 -0500

    Añadi comentarios de mis metodos

--- Busqueda lineal.py
+++ Busqueda lineal.py
@@ -1,11 +1,13 @@
 import time
 import random
-from typing import final
+
+# Algoritmo de Busqueda lineal
 def Busqueda(array, numero):
     for n in range(len(array)):
         if array[n] == numero:
             print("lo encontro")
 
+# Algoritmo insertionSort
 def insertionSort(array):
     for i in range(1,len(array)):
         aux = array[i]
@@ -16,19 +18,22 @@ def insertionSort(array):
         array[j + 1] = aux
     print(array)
 
-def n_values(n):
+# Generador de matriz con numeros aleatorios
+def n_values(n):  
     array=[ ]
     for i in range(n):
         array.append(random.randint(1,n))
     return array
 
+#Tiempo computacional usando la libreria time
 inicio = time.time()
-array = n_values(10000)      
-# Busqueda(array,numero)
+numero = 8
+array = n_values(10000)
+Busqueda(array,numero)
 insertionSort(array)
 final = time.time()
 
-print(final-inicio)
+print(inicio)
 
 
 
